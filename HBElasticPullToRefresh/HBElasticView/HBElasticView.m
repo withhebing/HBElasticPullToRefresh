b193754cc2b2cb2fcee27efa6fa0bc853813b8c3
Bean He <withhebing@gmail.com>
2016-08-18 00:51:40 +0800

    update gesture

--- HBElasticPullToRefresh/HBElasticView/HBElasticView.m
+++ HBElasticPullToRefresh/HBElasticView/HBElasticView.m
@@ -20,7 +20,12 @@
 
 // control point
 @property (nonatomic, strong) UIView *centerPoint;
-
+@property (nonatomic, strong) UIView *l1Point;
+@property (nonatomic, strong) UIView *l2Point;
+@property (nonatomic, strong) UIView *l3Point;
+@property (nonatomic, strong) UIView *r1Point;
+@property (nonatomic, strong) UIView *r2Point;
+@property (nonatomic, strong) UIView *r3Point;
 
 @end
 
@@ -65,8 +70,13 @@
     self = [super initWithFrame:frame];
     if (self) {
         // your codes
-        self.shapeLayer.fillColor = (__bridge CGColorRef _Nullable)([UIColor blueColor]);
-        self.centerPoint.backgroundColor = [UIColor redColor];
+//        self.shapeLayer.fillColor = (__bridge CGColorRef _Nullable)([UIColor blueColor]);
+//        self.centerPoint.backgroundColor = [UIColor redColor];
+
+        [self setupShapeLayer];
+        [self setupCenterPoint];
+        [self setupGuesture];
+        [self shapeLayerPath];
     }
     return self;
 }
@@ -75,6 +85,7 @@
     _shapeLayer = [[CAShapeLayer alloc] init];
     _shapeLayer.frame = CGRectMake(0, 0, kScreenW, minHeight);
     _shapeLayer.fillColor = (__bridge CGColorRef _Nullable)([UIColor blueColor]);
+    _shapeLayer.strokeColor = (__bridge CGColorRef _Nullable)([UIColor cyanColor]);
     [self.layer addSublayer:_shapeLayer];
 }
 
@@ -91,6 +102,7 @@
 
 - (void)panAction:(UIPanGestureRecognizer *)recognizer {
     if (recognizer.state == UIGestureRecognizerStateChanged) {
+        // 跟着触摸点移动
         CGPoint point = [recognizer translationInView:self];
         CGFloat centerPtX = kScreenW * 0.5 + point.x;
         self.minimalHeight = minHeight + point.y;
@@ -98,18 +110,33 @@
         self.centerPoint.frame = CGRectMake(centerPtX, centerPtY, 3.0, 3.0);
 
     } else if (recognizer.state == UIGestureRecognizerStateEnded || recognizer.state == UIGestureRecognizerStateCancelled || recognizer.state == UIGestureRecognizerStateFailed) {
-        //
+        // 弹簧效果 恢复原来位置
+        [UIView animateWithDuration:1.0 delay:0.0 usingSpringWithDamping:0.5 initialSpringVelocity:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            //
+            self.centerPoint.frame = CGRectMake(kScreenW * 0.5, minHeight, 3.0, 3.0);
+        } completion:^(BOOL finished) {
+            //
+        }];
     }
 }
 
 - (void)shapeLayerPath {
-    UIBezierPath *shapeLayerPath = [UIBezierPath bezierPath];
-    [shapeLayerPath moveToPoint:CGPointZero];
-    [shapeLayerPath addLineToPoint:CGPointMake(0, self.minimalHeight)];
-    [shapeLayerPath addQuadCurveToPoint:CGPointMake(kScreenW, self.minimalHeight) controlPoint:self.centerPoint.center];
-    [shapeLayerPath addLineToPoint:CGPointMake(kScreenW, 0)];
-    [shapeLayerPath closePath];
-    self.shapeLayer.path = shapeLayerPath.CGPath;
+//    UIBezierPath *shapeLayerPath = [UIBezierPath bezierPath];
+//    [shapeLayerPath moveToPoint:CGPointZero];
+//    [shapeLayerPath addLineToPoint:CGPointMake(0, self.minimalHeight)];
+//    [shapeLayerPath addQuadCurveToPoint:CGPointMake(kScreenW, self.minimalHeight) controlPoint:self.centerPoint.center];
+//    [shapeLayerPath addLineToPoint:CGPointMake(kScreenW, 0)];
+//    [shapeLayerPath closePath];
+//    self.shapeLayer.path = shapeLayerPath.CGPath;
+
+    UIBezierPath *tPath = [UIBezierPath bezierPath];
+    [tPath moveToPoint:CGPointMake(0, 0)];                              // r1点
+    [tPath addLineToPoint:CGPointMake(kScreenW, 0)];            // r2点
+    [tPath addLineToPoint:CGPointMake(kScreenW, minHeight)];  // r4点
+    [tPath addQuadCurveToPoint:CGPointMake(0, minHeight)
+                  controlPoint:self.centerPoint.center]; // r3,r4,r5确定的一个弧线
+    [tPath closePath];
+    self.shapeLayer.path = tPath.CGPath;
 }
 
 /*
